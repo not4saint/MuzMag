8ab586d1019334b8af75d28000a34c63179dd837
fromRandomUser <gg@mail.com>
2023-03-28 15:12:46 +0300

    commit init

---
+++ src/main/java/com/muztorg/MuzTorg/MuzTorgApplication.java
@@ -0,0 +1,13 @@
+package com.muztorg.MuzTorg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuzTorgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuzTorgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/muztorg/MuzTorg/MuzTorgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muztorg.MuzTorg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuzTorgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
